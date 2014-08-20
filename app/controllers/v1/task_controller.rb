4ad1512eddf263f209fdcf868ec7924520f7d0dd
Speed.Of.Light <speed.of.lightt@gmail.com>
2014-08-21 01:41:08 +0800

    Add task#list, task#sweep, and make task#show consume task_id

--- app/controllers/v1/task_controller.rb
+++ app/controllers/v1/task_controller.rb
@@ -4,13 +4,35 @@ class V1::TaskController < V1::BaseController
   self.cache_store = ActionController::Base.cache_store
 
   def show
-    task = cache [:show, params[:id]] do
-      task = Task.find(params[:id])
+    ck = "#{controller_name}.#{action_name}.#{params[:id]}"
+    task = Rails.cache.fetch ck do
+      y, r, t = params[:id].split('-')
+      task = Task.in_year(y.to_i)
+      task = task.where(region: r, tid: t).first
       TaskSerializer.new(task).to_json
     end
     render json: task
   end
 
   def list
+    # TODO sweep after create
+    ck = "#{controller_name}.#{action_name}"
+    tl = Rails.cache.fetch ck do
+      task_list_json Task.all
+    end
+    render json: tl
+  end
+
+  def sweep
+    sk = "#{controller_name}.show.#{params[:id]}"
+    Rails.cache.delete(sk)
+    render json: { status: "done" }
+  end
+
+  private
+  def task_list_json tasks
+    aas = ActiveModel::ArraySerializer
+    sts = SimpleTaskSerializer
+    aas.new(tasks, each_serializer: sts).to_json
   end
 end
