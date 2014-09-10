7997dac28f51fe55ffeb1fa4cdd28bc3c13a3254
Speed.Of.Light <speed.of.lightt@gmail.com>
2014-09-11 05:36:05 +0800

    fix msg priviledges

--- app/controllers/v1/dashboards/msgs_controller.rb
+++ app/controllers/v1/dashboards/msgs_controller.rb
@@ -1,4 +1,5 @@
 class V1::Dashboards::MsgsController < V1::BaseController
+  skip_before_filter :authenticate_user_from_token!, :only => [:index]
   def index
     msgs = Msg.all
     msgs = ActiveModel::ArraySerializer.new(msgs, each_serializer: MsgSerializer)
