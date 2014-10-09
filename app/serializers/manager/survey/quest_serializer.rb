70755b686186411d973e8e7c6b51ed9d3c142ba0
Speed.Of.Light <speed.of.lightt@gmail.com>
2014-10-09 09:14:20 +0800

    fix namespace issue in survey quest serializer

--- app/serializers/manager/survey/quest_serializer.rb
+++ app/serializers/manager/survey/quest_serializer.rb
@@ -1,5 +1,5 @@
 class Manager::Survey::QuestSerializer < ActiveModel::Serializer
   root false
   attributes :id, :order, :content, :qtype
-  has_many :choices, serializer: ChoiceSerializer
+  has_many :choices, serializer: Manager::Survey::ChoiceSerializer
 end
