33772dcb67e959367c6daa12d7e1b5ee17a428d4
Speed.Of.Light <speed.of.lightt@gmail.com>
2014-10-24 23:51:19 +0100

    add more file types

--- app/uploaders/share_uploader.rb
+++ app/uploaders/share_uploader.rb
@@ -35,7 +35,7 @@ class ShareUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
-    %w(jpg jpeg gif png doc docx pdf zip)
+    %w(jpg jpeg gif png doc docx pdf zip csv)
   end
 
   # Override the filename of the uploaded files:
